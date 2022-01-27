8c560385bf57d2859d0f1a73ad2e39fd4e94010f
iMAC_33 <imac_33@192.168.50.12>
2022-01-27 16:50:58 +0000

    ajout des fenetres des different bouton

--- Ges_stock.py
+++ Ges_stock.py
@@ -2,7 +2,9 @@
 
 from ctypes.wintypes import SIZE
 from distutils import command
+from math import prod
 from optparse import Values
+from queue import Empty
 from re import search
 import tkinter as tk
 from tkinter import*
@@ -14,20 +16,24 @@ from turtle import color, left, position, width
 
 
 window = Tk()
-window.geometry('1200x1200')
+window.geometry('2000x2000')
 window.title("GES-STOCK")
 window.configure(bg='white',background="gainsboro")
 
 
-
-
-Frame_window1 = LabelFrame(window,width=1500,height=70)
+Frame_window1 = LabelFrame(window,width=2000,height=70,padx=100)
 Frame_window1.configure(bg="white")
-Frame_window1.place(x=20,y=0)
+Frame_window1.place(x=153,y=0)
+
+# barre_rech = Label(Frame_window1,text="Rech/Email:",borderwidth=5,border=1,fg="red",bg="white")
+# barre_rech.configure(bg='lavender')
+# barre_rech.pack(side=tkinter.RIGHT,pady=5,padx=5)
+# Barre =Entry(Frame_window1,)
+# Barre.pack(side=tkinter.RIGHT,pady=5,padx=10)
 
 Frame_window2 = LabelFrame(window,width=150,height=1200,)
 Frame_window2.configure(bg="lavender")
-Frame_window2.pack(side=tkinter.LEFT,pady=0,padx=5)
+Frame_window2.pack(side=tkinter.LEFT,pady=5,padx=5)
 
 
 
@@ -46,25 +52,235 @@ imglabel = Label(Frame_img,image=img,width=1500,height=1000).place(x=5,y=10)
 
 
 def open_appr():
-   win = Toplevel(Frame_window3)
-   win.geometry(1200,1200)
+   win = Toplevel(Frame_window1)
+   win.geometry('1000x1000')
    win.title("CLIENT")
-   win.configure(bg="red")
-   
-   
-   
-   
+   win.configure(bg="white")
+
+   Frame_1 = LabelFrame(win,width=1500,height=80,bg="lavender")
+   Frame_1.configure(bg="lavender")
+   Frame_1.pack(side=tkinter.TOP,pady=5,padx=0)
+
+   Frame2 = LabelFrame(win,text="Formulaire de saisie")
+   Frame2.configure(bg="white")
+   Frame2.pack(side=tkinter.TOP, padx=5,pady=5, ipady=50, ipadx=30)
+
+   treeframe = LabelFrame(win,width=1200,height=1000)
+   treeframe.configure(bg="white")
+   treeframe.pack(pady=10,padx=10)
+
+   column = ["Nom produit","Code","Qte_livrée","PA unitaire","Prix unitaire","PA global","Fournisseur"]
+
+   data =[
+      ('f65','pomme','500','500','fj','gnjg','hgjg'),
+      ('f65','miment','1000','500','fj','gnjg','hgjg'),
+      ('f65','oignon','400','500','fj','gnjg','hgjg'),
+      ('f65','pomme','2460','500','fj','gnjg','hgjg'),
+      ('f65','gombo','5620','500','fj','gnjg','hgjg'),
+         
+   ]
+   myTree = ttk.Treeview(treeframe,height=200,column=column)
+   myTree.pack(fill="both")
+   myTree['show']='headings'
+   for each in column:
+      myTree.column(each,width=90)
+      myTree.heading(each,text=each.capitalize())
+   for each in data:
+      myTree.insert("",END,values=each)
+
+
+
+   Produit = Label(Frame2,text= "Nom produit:")
+   Produit.grid(row=1,column=1)
+   Produit = Entry(Frame2,border=2)
+   Produit.grid(row=1,column=2)
+
+   Code = Label(Frame2,text= "Code:")
+   Code.grid(row=2,column=1)
+   Code =Entry(Frame2,border=2)
+   Code.grid(row=2,column=2)
+
+   Qte_livree = Label(Frame2,text= "Qte_livrée:")
+   Qte_livree.grid(row=3,column=1)
+   Qte_livree = Entry(Frame2,border=2)
+   Qte_livree.grid(row=3,column=2)
+
+   Prix_unitaire = Label(Frame2,text= "PA unitaire:")
+   Prix_unitaire.grid(row=4,column=1)
+   Prix_unitaire = Entry(Frame2,border=2)
+   Prix_unitaire.grid(row=4,column=2)
+
+   Prix_global = Label(Frame2,text= "PA global:")
+   Prix_global.grid(row=5,column=1)
+   Prix_global = Entry(Frame2,border=2)
+   Prix_global.grid(row=5,column=2)
+
+   Fournisseur = Label(Frame2,text= "Fournisseur:")
+   Fournisseur.grid(row=6,column=1)
+   Fournisseur = Entry(Frame2,border=2)
+   Fournisseur.grid(row=6,column=2)
+
+def open_vente():
+       
+   win = Toplevel(Frame_window1)
+   win.geometry('1000x1000')
+   win.title("CLIENT")
+   win.configure(bg="white")
+
+   Frame_1 = LabelFrame(win,width=1500,height=80,bg="lavender")
+   Frame_1.configure(bg="white")
+   Frame_1.pack(side=tkinter.TOP,pady=5,padx=0)
+
+   Frame2 = LabelFrame(win,text="Formulaire de saisie")
+   Frame2.configure(bg="white")
+   Frame2.pack(side=tkinter.TOP, padx=5,pady=5, ipady=50, ipadx=30)
+
+   treeframe = LabelFrame(win,width=1200,height=1000)
+   treeframe.configure(bg="white")
+   treeframe.pack(pady=10,padx=10)
+
+   column = ["Nom produit","Code","Qte_livrée","PA unitaire","Prix unitaire","PA global","Fournisseur"]
+
+   data =[
+      ('f65','pomme','500','500','fj','gnjg','hgjg'),
+      ('f65','miment','1000','500','fj','gnjg','hgjg'),
+      ('f65','oignon','400','500','fj','gnjg','hgjg'),
+      ('f65','pomme','2460','500','fj','gnjg','hgjg'),
+      ('f65','gombo','5620','500','fj','gnjg','hgjg'),
+         
+   ]
+   myTree = ttk.Treeview(treeframe,height=200,column=column)
+   myTree.pack(fill="both")
+   myTree['show']='headings'
+   for each in column:
+      myTree.column(each,width=90)
+      myTree.heading(each,text=each.capitalize())
+   for each in data:
+      myTree.insert("",END,values=each)
+
+
+   Produit = Label(Frame2,text= "Nom produit:")
+   Produit.grid(row=1,column=1)
+   Produit = Entry(Frame2,border=2)
+   Produit.grid(row=1,column=2)
+
+   Code = Label(Frame2,text= "Qte:")
+   Code.grid(row=2,column=1)
+   Code =Entry(Frame2,border=2)
+   Code.grid(row=2,column=2)
+
+   Qte_livree = Label(Frame2,text= "Montant:")
+   Qte_livree.grid(row=3,column=1)
+   Qte_livree = Entry(Frame2,border=2)
+   Qte_livree.grid(row=3,column=2)
+
+   Prix_unitaire = Label(Frame2,text= "PA unitaire:")
+   Prix_unitaire.grid(row=4,column=1)
+   Prix_unitaire = Entry(Frame2,border=2)
+   Prix_unitaire.grid(row=4,column=2)
+
+   Prix_global = Label(Frame2,text= "PA global:")
+   Prix_global.grid(row=5,column=1)
+   Prix_global = Entry(Frame2,border=2)
+   Prix_global.grid(row=5,column=2)
+
+   Fournisseur = Label(Frame2,text= "Fournisseur:")
+   Fournisseur.grid(row=6,column=1)
+   Fournisseur = Entry(Frame2,border=2)
+   Fournisseur.grid(row=6,column=2)
+
+
+def open_stock():
+       
+   win = Toplevel(Frame_window1)
+   win.geometry('1000x1000')
+   win.title("CLIENT")
+   win.configure(bg="white")
+
+   Frame_1 = LabelFrame(win,width=1500,height=80,bg="lavender")
+   Frame_1.configure(bg="white")
+   Frame_1.pack(side=tkinter.TOP,pady=5,padx=0)
+
+   Frame2 = LabelFrame(win,text="Formulaire de saisie")
+   Frame2.configure(bg="white")
+   Frame2.pack(side=tkinter.TOP, padx=5,pady=5, ipady=50, ipadx=30)
+
+   treeframe = LabelFrame(win,width=1200,height=1000)
+   treeframe.configure(bg="white")
+   treeframe.pack(pady=10,padx=10)
+
+   column = ["Nom produit","Code","Qte_livrée","PA unitaire","Prix unitaire","PA global","Fournisseur"]
+
+   data =[
+      ('f65','pomme','500','500','fj','gnjg','hgjg'),
+      ('f65','miment','1000','500','fj','gnjg','hgjg'),
+      ('f65','oignon','400','500','fj','gnjg','hgjg'),
+      ('f65','pomme','2460','500','fj','gnjg','hgjg'),
+      ('f65','gombo','5620','500','fj','gnjg','hgjg'),
+         
+   ]
+   myTree = ttk.Treeview(treeframe,height=200,column=column)
+   myTree.pack(fill="both")
+   myTree['show']='headings'
+   for each in column:
+      myTree.column(each,width=90)
+      myTree.heading(each,text=each.capitalize())
+   for each in data:
+      myTree.insert("",END,values=each)
+
+
+   Produit = Label(Frame2,text= "Nom produit:")
+   Produit.grid(row=1,column=1)
+   Produit = Entry(Frame2,border=2)
+   Produit.grid(row=1,column=2)
+
+   Code = Label(Frame2,text= "Code:")
+   Code.grid(row=2,column=1)
+   Code =Entry(Frame2,border=2)
+   Code.grid(row=2,column=2)
+
+   Qte_livree = Label(Frame2,text= "Qte_livrée:")
+   Qte_livree.grid(row=3,column=1)
+   Qte_livree = Entry(Frame2,border=2)
+   Qte_livree.grid(row=3,column=2)
+
+   Prix_unitaire = Label(Frame2,text= "PA unitaire:")
+   Prix_unitaire.grid(row=4,column=1)
+   Prix_unitaire = Entry(Frame2,border=2)
+   Prix_unitaire.grid(row=4,column=2)
+
+   Prix_global = Label(Frame2,text= "PA global:")
+   Prix_global.grid(row=5,column=1)
+   Prix_global = Entry(Frame2,border=2)
+   Prix_global.grid(row=5,column=2)
+
+   Fournisseur = Label(Frame2,text= "Fournisseur:")
+   Fournisseur.grid(row=6,column=1)
+   Fournisseur = Entry(Frame2,border=2)
+   Fournisseur.grid(row=6,column=2)
+       
+       
+        
 photo1 = PhotoImage(file="add.png")
 photoimage1 = photo1.subsample(10,10)
 Button(Frame_window2,text="Approv",image=photoimage1,compound=BOTTOM,command=open_appr).place(x=15,y=30)
 
 photo2 = PhotoImage(file="add.png")
 photoimage2 = photo2.subsample(10,10)
-Button(Frame_window2,text="Ventes",image=photoimage2,compound=BOTTOM,borderwidth=2).place(x=15,y=100)
+Button(Frame_window2,text="Ventes",image=photoimage2,compound=BOTTOM,command=open_vente,borderwidth=2).place(x=15,y=100)
 
 photo3 = PhotoImage(file="stock.png")
 photoimage3 = photo3.subsample(8,10)
 Button(Frame_window2,text="Stock",image=photoimage3,compound=BOTTOM).place(x=15,y=170)
+
+photo4 = PhotoImage(file="stock.png")
+photoimage4 = photo4.subsample(8,10)
+Button(Frame_window2,text="F/ss",image=photoimage4,compound=BOTTOM).place(x=15,y=240)
+
+photo5 = PhotoImage(file="stock.png")
+photoimage5 = photo5.subsample(8,10)
+Button(Frame_window1,text="F/ss",image=photoimage5,compound=TOP).place(x=15,y=240)
+
 
 
 
